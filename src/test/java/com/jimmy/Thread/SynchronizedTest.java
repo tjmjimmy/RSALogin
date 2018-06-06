a8e6229de6f3e0a6b98142fb9b915128b4b01859
tianjm <919769936@qq.com>
2018-06-06 22:46:10 +0800

    new

--- src/test/java/com/jimmy/Thread/SynchronizedTest.java
+++ src/test/java/com/jimmy/Thread/SynchronizedTest.java
@@ -6,33 +6,98 @@ public class SynchronizedTest {
 	 * */
 	public static void main(String[] args){
 		
-		Print p = new Print();
+		Print p1 = new Print();
+		Print p2 = new Print();
 		
-		Thread th1 = new Thread(new Runnable() {
+		SynClazz sc1 = new SynClazz();
+		SynClazz sc2 = new SynClazz();
+		/*
+		Thread th1 = new Thread("thread1"){
 			
 			@Override
 			public void run() {
-				// TODO Auto-generated method stub
-				p.printArray();
+				p1.printArray();
 			}
-		}, "thread1");
-		Thread th2 = new Thread(new Runnable() {
+		};
+		Thread th2 = new Thread("thread2"){
 			
 			@Override
 			public void run() {
-				// TODO Auto-generated method stub
-				p.synchronizedPrint();
+				p2.synchronizedPrint();
 			}
-		}, "thread2");
+		};
+		Thread th3 = new Thread("thread3"){
+			@Override
+			public void run() {
+				Print.staicSynchronizedPrint();
+			}
+		};
+		*/		
+		/*Thread th4 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				sc1.print();
+				
+			}
+		}, "thread4");
 		
-		th1.start();
-		th2.start();
+		Thread th5 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				sc2.print();
+				
+			}
+		}, "thread5");*/
+		
+		/*Thread th6 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				p1.synClazzPrintArgs(sc1);				
+			}
+		}, "thread6");*/
+
+		Thread th7 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				SynClazz.statiIincrement();				
+			}
+		}, "thread7");
+
+		Thread th8 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				sc1.increment();
+			}
+		}, "thread8");
+		
+//		th1.start();
+//		th2.start();
+//		th3.start();		
+//		th4.start();
+//		th5.start();
+//		th6.start();
+		th7.start();
+		th8.start();
 	}	
 
 }
 
 class Print{
-	Object o = new Object();
+	private Object o = new Object();
+	
+	void ordinaryPrint(){
+		int sum = 0;
+		for(int i = 0; i < 100; i++){
+			sum += i;
+			System.out.println(Thread.currentThread().getName() + ">>>sum=" + sum);
+		}
+	}
+	
 	void printArray(){
 		/**
 		 * 在synchronized()中new Object()并不能实现synchronized效果，因为在不同线程中调用该方法，synchronized()锁住的是不同的对象，都是新new出来的
@@ -40,7 +105,7 @@ class Print{
 		 * 将下行该为synchronized(o)，输出结果就不一样
 		 * */
 		/**
-		 * 在new tmp()构造方法中，虽然使用了静态成员变量，但synchronized(new tmp())加的是tmp的对象锁，不是tmp类锁，所以也不会造成阻塞
+		 * 在new temp()构造方法中，虽然使用了静态成员变量，但synchronized(new temp())加的是temp的对象锁，不是temp类锁，所以也不会造成阻塞
 		 * */
 		/**
 		 * 如果synchronized(o)，再用不同线程执行printArray()和synchronizedPrint()也不会实现synchronized的效果，
@@ -55,7 +120,45 @@ class Print{
 		}		
 	}
 	
+	void synClazzPrint(){
+		synchronized (SynClazz.class) {
+			int sum = 0;
+			for(int i = 0; i < 100; i++){
+				sum += i;
+				System.out.println(Thread.currentThread().getName() + ">>>sum=" + sum);
+			}
+		}
+	}
+
+	void synClazzPrintArgs(SynClazz sc){
+		synchronized (SynClazz.class) {
+			sc.increment();
+		}
+	}
+	
+	void synClazzPrintArgs(){
+		synchronized (SynClazz.class) {
+			SynClazz.statiIincrement();
+		}
+	}
+	
+	
+	/**
+	 * synchronized方法占用的是对象锁，哪个对象调用该方法锁住的就是哪个对象，
+	 * 和synchronized（this）一样，占用的都是对象锁
+	 * */
 	synchronized void synchronizedPrint(){
+		int sum = 0;
+		for(int i = 0; i < 100; i++){
+			sum += i;
+			System.out.println(Thread.currentThread().getName() + ">>>sum=" + sum);
+		}
+	}
+	
+	/**
+	 * static synchronized修饰的方法占用的是类锁
+	 * */
+	static synchronized void staicSynchronizedPrint(){
 		int sum = 0;
 		for(int i = 0; i < 100; i++){
 			sum += i;
@@ -65,9 +168,40 @@ class Print{
 	
 }
 
-class tmp{
+class SynClazz {
+
+	private Print p = new Print();
+	
+	void print(){
+		p.synClazzPrint();
+	}
+	
+	void increment(){
+		synchronized (this) {
+			int sum = 0;
+			for(int i = 0; i < 50; i++){
+				sum += i;
+				System.out.println(Thread.currentThread().getName() + ">>>sum=" + sum);
+			}
+			
+		}
+	}
+
+	static void statiIincrement(){
+		synchronized (SynClazz.class) {
+			int sum = 0;
+			for(int i = 0; i < 50; i++){
+				sum += i;
+				System.out.println(Thread.currentThread().getName() + ">>>sum=" + sum);
+			}
+			
+		}
+	}
+}
+
+class temp{
 	static int i = 0;
-	tmp(){
+	temp(){
 		System.out.println(Thread.currentThread().getName() + ">>>" + this.hashCode() + ">>i=" + i);
 	}
 }
