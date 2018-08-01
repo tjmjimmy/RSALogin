7f5c9919536dd0b1eab700e13a58be8239b1e346
tjmjimmy <919769936@qq.com>
2018-08-01 14:20:58 +0800

    shiro测试

--- src/test/java/com/jimmy/shiro/chapter2/LoginLogoutTest.java
+++ src/test/java/com/jimmy/shiro/chapter2/LoginLogoutTest.java
@@ -38,4 +38,29 @@ public class LoginLogoutTest {
 		logger.info("校验成功");
 		subject.logout();
 	}
+	
+	@Test
+	public void testRealm(){
+		
+		logger.info("testRealm()方法开始");
+		
+		Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-realm.ini"); 
+		
+		SecurityManager securityManager = factory.getInstance();
+		SecurityUtils.setSecurityManager(securityManager);
+		
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken("jimmy", "jimmy123456");
+		
+		try {
+			logger.info("开始校验");
+			subject.login(token);
+		} catch (AuthenticationException e) {
+			logger.error(e.getMessage(), e);
+		}
+		
+		Assert.assertEquals(true, subject.isAuthenticated());
+		logger.info("校验成功");
+		subject.logout();
+	}
 }
